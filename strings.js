fe93366219bc01fb9dd8175cb56501f451f25acb
PWMUAT <133041113+PWMUAT@users.noreply.github.com>
2023-06-19 20:50:07 -0700

    Add files via upload

---
+++ strings.js
@@ -0,0 +1,46 @@
+const firstname = document.getElementById("firstname");
+const lastname = document.getElementById("lastname");
+const zipcode = document.getElementById("zipcode");
+const form = document.querySelector("form");
+const errorMessage = document.getElementById("errorMessage");
+
+
+form.addEventListener("submit", (e) => {
+    //array for storing errors
+    const errors = [];
+
+    //check for only spaces in input
+    if(firstname.value.trim() === "") {
+        errors.push("First Name Required")
+    }
+    //check for only spaces in input
+    if(lastname.value.trim() === "") {
+        errors.push("Last Name Required")
+    }
+    //check for only spaces in input
+    if(zipcode.value.trim() === "") {
+        errors.push("Zip Code Required")
+    }
+
+    //creating a variable with both names
+    const fullName = firstname.value + " " + lastname.value;
+
+    //well screw anyone with a long name I guess, rude
+    if(fullName.length > 21) {
+        errors.push("More than 20 characters!")
+    }
+    //checking zipcode input length
+    if(zipcode.value.length != 5) {
+        errors.push("Zip Code must be 5 characters long!")
+    }
+    //output errors if any occurr
+    if(errors.length > 0) {
+        e.preventDefault();
+        errorMessage.toggleAttribute('hidden');
+        errorMessage.innerHTML = errors.join(', ');
+    }
+    //if no errors, display "secret" message
+    else {
+        alert("Hello " + fullName + "!"); 
+    }
+});
