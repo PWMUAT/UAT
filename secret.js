2f374902f65bfb5781e2c1efe27adc4175bf5525
PWMUAT <133041113+PWMUAT@users.noreply.github.com>
2023-07-30 15:32:39 -0700

    Assignment 12.1 fix

--- secret.js
+++ secret.js
@@ -11,13 +11,16 @@ function trySecret() {
         userInput = userInput.replace(/\s/g, "");
     } while (userInput.trim() === "");
 
-    //
+    //validate if password is correct
     if(userInput === secretPassword) {
+        //log and alert result
         alert("Right!");
         console.log("Oh, they know the password.");
+        //unhide image
         document.getElementById("SecretImage").hidden = false;
     }
     else {
+        //log and alert result
         alert("WRONG!");
         console.log("THEY DON'T KNOW THE PASSWORD");
     }
